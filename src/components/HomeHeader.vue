4db3ba2acb71390d30237ca2fb7b105f118385c8
亂懟 <2511030361@qq.com>
2019-10-13 18:17:34 +0800

    添加header区域

---
+++ src/components/HomeHeader.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      <span class="iconfont back-icon">&#xe624;</span>
+    </div>
+    <div class="header-input">
+      <span class="iconfont">&#xe632;</span>
+      输入城市/景点/游玩主题
+    </div>
+    <div class="header-right">
+      城市
+      <span class="iconfont arrow-icon">&#xe600;</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~@styles/varible.styl'
+  .header
+    display: flex
+    line-height: .86rem
+    background-color: $bgColor
+    color: #ffffff
+    .header-left
+      width: .64rem
+      float: left
+      .back-icon
+        display: block
+        text-align: center
+        font-size: .4rem
+    .header-input
+      flex: 1
+      height: .64rem
+      line-height: .64rem
+      margin-top: .12rem
+      margin-left: .2rem
+      padding-left: .2rem
+      background: #ffffff
+      border-radius: .1rem
+      color: #cccccc;
+    .header-right
+      width: 1.24rem
+      float: right
+      text-align: center
+      .arrow-icon
+        font-size: .24rem
+        margin-left: -.04rem
+</style>
