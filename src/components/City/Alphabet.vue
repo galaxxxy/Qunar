89f2ccaa590c1267855d935babd6116c7eadb969
亂懟 <2511030361@qq.com>
2019-10-15 21:15:54 +0800

    performance optimization

--- src/components/City/Alphabet.vue
+++ src/components/City/Alphabet.vue
@@ -23,25 +23,36 @@ export default {
   },
   data () {
     return {
-      touchStatus: false
+      touchStatus: false,
+      startY: 0,
+      timer: null
     }
+  },
+  updated () {
+    this.startY = this.$refs['A'][0].offsetTop
   },
   methods: {
     handleLetterClick (e) {
       this.$emit('change', e.target.innerText)
     },
-    handleTouchStart (e) {
+    handleTouchStart () {
       this.touchStatus = true
     },
     handleTouchMove (e) {
-      const startY = this.$refs['A'][0].offsetTop
-      const touchY = e.touches[0].clientY - 79
-      const index = Math.floor((touchY - startY) / 20)
-      if (index >= 0 || index < this.letters.length) {
-        this.$emit('change', this.letters[index])
+      if (this.touchStatus) {
+        if (this.timer) {
+          clearTimeout(this.timer)
+        }
+        this.timer = setTimeout(() => {
+          const touchY = e.touches[0].clientY - 79
+          const index = Math.floor((touchY - this.startY) / 20)
+          if (index >= 0 && index < this.letters.length) {
+            this.$emit('change', this.letters[index])
+          }
+        }, 6)
       }
     },
-    handleTouchEnd (e) {
+    handleTouchEnd () {
       this.touchStatus = false
     }
   },
@@ -59,7 +70,7 @@ export default {
   .list
     display: flex
     flex-direction: column
-    justify-content center
+    justify-content: center
     position: absolute
     top: 1.58rem
     bottom: 0
