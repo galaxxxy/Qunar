9f293bd6e99fe87449a20c10177d013c1d2cf8b1
亂懟 <2511030361@qq.com>
2019-10-15 18:11:21 +0800

    add searchbox to city view

--- src/views/City.vue
+++ src/views/City.vue
@@ -1,15 +1,18 @@
 <template>
 <div>
-<city-header></city-header>
+  <city-header></city-header>
+  <city-search></city-search>
 </div>
 </template>
 
 <script>
 import CityHeader from '@/components/City/Header'
+import CitySearch from '@/components/City/Search'
 export default {
   name: 'City',
   components: {
-    CityHeader
+    CityHeader,
+    CitySearch
   }
 }
 </script>
